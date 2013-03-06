10e8d40c4f4f8872fca40d06589645151c3a26a4
Jim Evans <james.h.evans.jr@gmail.com>
2013-03-05 20:46:10 -0500

    Simplifying script to readlist of packages to download and install from data file.

--- download.cmd
+++
@@ -1,7 +0,0 @@
-if not exist packages md packages
-bin\curl -o packages\Slik-subversion.msi http://www.sliksvn.com/pub/Slik-Subversion-1.7.6-win32.msi
-bin\curl -o packages\python.msi http://python.org/ftp/python/2.7.3/python-2.7.3.msi
-bin\curl -o packages\rubyinstaller.exe http://rubyforge.org/frs/download.php/76054/rubyinstaller-1.9.3-p194.exe/noredirect
-bin\curl -o packages\jdk.exe -k -L -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F" http://download.oracle.com/otn-pub/java/jdk/6u35-b10/jdk-6u35-windows-i586.exe
-bin\curl -o packages\dotnetfx35.exe http://download.microsoft.com/download/2/0/E/20E90413-712F-438C-988E-FDAA79A8AC3D/dotnetfx35.exe
-bin\curl -o packages\dotNetFx40_Full_x86_x64.exe http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe
