37cb49436f850db187476a7288b4b54590328151
jimevans <james.h.evans.jr@gmail.com>
2018-07-23 14:08:32 -0400

    Updating VS 2017 install command to include correct params

--- go.cmd
+++ go.cmd
@@ -10,4 +10,4 @@ REM .NET Framework 4.0 is a prerequisite for chocolatey,
 REM so is unneeded as a separate install.
 REM choco install -y dotnet4.0
 
-if [%1]==[addvs] choco install -y visualstudio2017community -packageParameters "--Features MDDCPlusPlus"
+if [%1]==[addvs] choco install -y visualstudio2017community --package-parameters "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools"
